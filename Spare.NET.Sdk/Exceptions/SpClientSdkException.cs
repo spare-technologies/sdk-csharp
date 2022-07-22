3d4ebf0c35ab1de1bc8df3dfd56ec4231256720e
hansimehdi <hasnimehdi@outlook.com>
2022-07-23 01:40:38 +0200

    :alien: Impelemnting code test and analysis CI flow

--- Spare.NET.Sdk/Exceptions/SpClientSdkException.cs
+++ Spare.NET.Sdk/Exceptions/SpClientSdkException.cs
@@ -1,15 +1,24 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace Spare.NET.Sdk.Exceptions
 {
     [Serializable]
-    public class SpClientSdkException : Exception
+    internal sealed class SpClientSdkException : Exception
     {
         public SpClientSdkException()
         {
         }
 
         public SpClientSdkException(string message) : base(message)
+        {
+        }
+
+        public SpClientSdkException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public SpClientSdkException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
--- Spare.NET.Sdk/Exceptions/SpNullReferenceException.cs
+++ Spare.NET.Sdk/Exceptions/SpNullReferenceException.cs
@@ -1,15 +1,24 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace Spare.NET.Sdk.Exceptions
 {
     [Serializable]
-    public class SpNullReferenceException : Exception
+    internal sealed class SpNullReferenceException : Exception
     {
         public SpNullReferenceException()
         {
         }
 
         public SpNullReferenceException(string message) : base(message)
+        {
+        }
+
+        public SpNullReferenceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public SpNullReferenceException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
